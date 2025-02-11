748b6530790efb98a5ed6d4cbeb1dca7983df670
Jesse <jesse@email.com>
2025-02-11 12:07:35 +0100

    Finished singleton pattern assignment

---
+++ LoggerSingletonPattern/LoggerSingletonPattern/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerSingletonPattern
+{
+    public class Logger
+    {
+        private static Logger? _instance = null;
+
+        private Logger()
+        {
+            
+        }
+
+        public static Logger GetInstance()
+        {
+            _instance ??= new Logger();
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ LoggerSingletonPattern/LoggerSingletonPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using LoggerSingletonPattern;
+
+Logger logger1 = Logger.GetInstance();
+Logger logger2 = Logger.GetInstance();
+
+Console.WriteLine(logger1 == logger2);
+
+logger1.Log("A log message");
+logger2.Log("Another log message");
